89bcaeeaa6ecae8e5856b8b5a631e4a0b28b2f54
Nuo Xu <xv.nuo@icloud.com>
2025-03-06 15:26:02 -0500

    add protein_id

--- scripts/add_exon_number_orfanage_gtf.py
+++ scripts/add_exon_number_orfanage_gtf.py
@@ -1,14 +1,19 @@
 from src.utils import read_gtf
 import polars as pl
+from src.utils import collapse_isoforms_to_proteoforms
 import polars.selectors as cs
 
 orfanage_gtf = read_gtf("nextflow_results/V47/orfanage/orfanage.gtf")
+
+# Add attribute "exon_number" to each exon
 
 exons = orfanage_gtf\
     .filter(pl.col("feature") == "exon")\
     .with_columns(
         exon_number = pl.col("transcript_id").rank(method="ordinal").over("transcript_id")
     )
+
+# Add attribute "exon_number" to each CDS, start_codon, and stop_codon
 
 other_level2_features = orfanage_gtf\
     .filter(
@@ -25,12 +30,28 @@ other_level2_features = orfanage_gtf\
         cs.ends_with("_right")
     )
 
+# Combine the two dataframes
+
 orfanage_gtf_with_exon_num = pl.concat([exons, other_level2_features])\
     .sort(["transcript_id", "start"])\
     .with_columns(exon_number = pl.col("exon_number").cast(pl.String))\
     .with_columns(
         attributes = pl.col("attributes") + pl.lit(' exon_number "') + pl.col("exon_number") + pl.lit('";')
-    ).drop(["transcript_id", "exon_number"])
+    ).drop("exon_number")
 
-orfanage_gtf_with_exon_num.write_csv("export/orfanage_with_exon_number.gtf", separator="\t", include_header=False, quote_style="never")
+# Add protein_id
 
+isoforms_to_proteoforms = collapse_isoforms_to_proteoforms(orfanage_gtf)
+
+orfanage_gtf_with_exon_num_protein_id = orfanage_gtf_with_exon_num\
+    .join(
+        isoforms_to_proteoforms.rename({"isoform": "transcript_id"}),
+        on = "transcript_id",
+        how = "left"
+    )\
+    .rename({"base_isoform": "protein_id"})\
+    .with_columns(
+        attributes = pl.col("attributes") + pl.lit(' protein_id "') + pl.col("protein_id") + pl.lit('";')
+    ).drop(["protein_id", "transcript_id"])
+
+orfanage_gtf_with_exon_num_protein_id.write_csv("export/orfanage_with_exon_number.gtf", separator="\t", include_header=False, quote_style="never")
