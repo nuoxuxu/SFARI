f66534551f6b7ed89633302550b8a8f59dfe593a
Nuo Xu <xv.nuo@icloud.com>
2025-03-24 09:51:06 -0400

    Add R script for generating correlation matrix of short and long read expression data where short-reads mapped to PB IDs

---
+++ scripts/figures/supplementary/short_long_read_correlation.R
@@ -0,0 +1,65 @@
+library(ggplot2)
+library(tximport)
+library(stringr)
+library(purrr)
+library(GenomicFeatures)
+library(arrow)
+library(dplyr)
+library(RColorBrewer)
+library(pheatmap)
+
+txdb <- makeTxDbFromGFF("nextflow_results/V47/final_transcripts.gtf")
+
+tx2gene <- select(txdb, keys = transcripts(txdb)$tx_name, columns = "GENEID", keytype = "TXNAME")
+
+files <- Sys.glob("nextflow_results/salmon_results/*/quant.sf")
+
+names(files) <- str_split(files, "/") %>%
+    map_chr(3)
+
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene, countsFromAbundance = "scaledTPM", txOut = TRUE, txIdCol = "isoform")
+
+sr_expression <- txi$counts %>% as_tibble(rownames = "isoform")
+
+lr_expression <- read_parquet("nextflow_results/V47/final_expression.parquet") %>% 
+    rename(
+        NPC_1_2 = NPC_1_3,
+        NPC_3_2 = NPC_3_3,
+        CN_1_1 = CN_1_2,
+        CN_1_2 = CN_1_3
+    )
+
+cor_matrix <- sr_expression %>%
+    left_join(lr_expression, join_by(isoform == isoform), suffix = c("", "_illumina")) %>%
+    select(where(is.numeric)) %>%
+    cor()
+
+sample_names <- c("iPSC_1", "iPSC_2", "iPSC_3", "CN_1_1", "CN_1_2", "CN_2_1", "CN_2_2", "CN_3_1", "CN_3_2", "NPC_1_1", "NPC_1_2", "NPC_2_1", "NPC_2_2", "NPC_3_1", "NPC_3_2")
+sample_names <- c(sample_names, str_c(sample_names, "_illumina"))
+
+cor_matrix <- cor_matrix[sample_names, sample_names]
+
+annotation <- data.frame(
+    row.names = rownames(cor_matrix),
+    time_point = str_extract(str_remove(rownames(cor_matrix), "_illumina"), "^[^_]*"),
+    technology = c(rep("pacbio", 15), rep("illumina", 15))
+)
+
+pheatmap(
+    cor_matrix,
+    color = colorRampPalette(brewer.pal(n = 7, name = "Reds"))(10),
+    cluster_rows = FALSE,
+    cluster_cols = FALSE,
+    annotation_col = annotation,
+    annotation_row = annotation,
+    show_rownames = FALSE,
+    show_colnames = FALSE,
+    display_numbers = TRUE,
+    number_color = "black",
+    fontsize_number = 6,
+    fontsize_row = 10,
+    fontsize_col = 10,
+    width = 10,
+    height = 10,
+    filename = "figures/supplementary/correlation_matrix_transcript.png"
+    )
