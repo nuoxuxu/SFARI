ea4ca0ec1de197489db722b6afdac165b59734fd
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-04-13 23:42:07 -0400

    Update Gviz.R script with new library imports and track modifications

--- scripts/figures/figure_2/Gviz.R
+++ scripts/figures/figure_2/Gviz.R
@@ -1,11 +1,11 @@
 library(Gviz)
 library(GenomicFeatures)
 library(GenomicRanges)
-library(GenomicInteractions)
 library(glue)
+library(tidyr)
 library(rtracklayer)
 library(dplyr)
-library(data.table)
+library(arrow)
 
 # Settings
 
@@ -20,7 +20,6 @@ addScheme(scheme, "myScheme")
 # Current gene
 
 current_gene <- "UGP2"
-novel_pb_id <- "PB.11316.259"
 
 # GENCODE track
 
@@ -28,9 +27,14 @@ GENCODE_GRList <- paste0(Sys.getenv("GENOMIC_DATA_DIR"), "/GENCODE/gencode.v47.a
   makeTxDbFromGFF(format = "gtf") %>%
   exonsBy(by = "tx", use.names = TRUE)
 
-tx_to_plot <- read.delim("export/plotgene_transcripts_codingOnly.txt", header = F) %>%
-  filter(V2 == current_gene, V5 == "gencode") %>%
-  pull(V3)
+tx_to_plot <- rtracklayer::import(paste0(Sys.getenv("GENOMIC_DATA_DIR"), "/GENCODE/gencode.v47.annotation.gtf")) %>% 
+  as_tibble() %>%
+  filter(
+    gene_name == current_gene
+  ) %>%
+  distinct(transcript_id) %>%
+  drop_na() %>%
+  pull(transcript_id)
 
 gencode_track <- GENCODE_GRList[tx_to_plot, ] %>%
   unlist() %>%
@@ -50,30 +54,6 @@ displayPars(gencode_track) <- list(
   background.title = "#d1861d"
 )
 
-# Isoseq track
-
-isoseq_gr <- "nextflow_results/V47/orfanage/orfanage.gtf" %>%
-  makeTxDbFromGFF(format = "gtf") %>%
-  exonsBy(by = "tx", use.names = TRUE) %>%
-  .[novel] %>%
-  unlist()
-
-elementMetadata(isoseq_gr)$transcript <- names(isoseq_gr)
-isoseq_track <- GeneRegionTrack(isoseq_gr, group = "transcript", name = "Novel")
-
-displayPars(isoseq_track) <- list(
-  stacking = "squish",
-  fill = "#E69F00",
-  col = "#E69F00",
-  lwd = 0.3,
-  col.line = "black",
-  showId = TRUE,
-  background.panel = "transparent",
-  transcriptAnnotation = "transcript",
-  fontcolor.title = "black",
-  background.title = "#045a8d"
-)
-
 # Peptide track
 
 peptide_annot <- rtracklayer::import("nextflow_results/V47/orfanage/annot_peptides_hybrid.gtf") %>% as_tibble()
@@ -85,8 +65,7 @@ peptides <- peptide_annot %>%
     novelty == "novel"
   ) %>%
   pull(transcript_id) %>%
-  unique() %>%
-  .[1]
+  unique()
 
 peptide_gr <- makeTxDbFromGFF("nextflow_results/V47/orfanage/annot_peptides_hybrid.gtf", format = "gtf") %>%
   exonsBy(by = "tx", use.names = TRUE) %>%
@@ -109,16 +88,41 @@ displayPars(peptide_track) <- list(
   background.title = "#ef6548"
 )
 
+# Isoseq track
+
+novel_pb_id <- read_parquet("nextflow_results/V47/orfanage/peptide_mapping.parquet") %>%
+  filter(peptide == peptides) %>% 
+  pull(pb)
+
+isoseq_gr <- "nextflow_results/V47/orfanage/orfanage.gtf" %>%
+  makeTxDbFromGFF(format = "gtf") %>%
+  exonsBy(by = "tx", use.names = TRUE) %>%
+  .[novel_pb_id] %>%
+  unlist()
+
+elementMetadata(isoseq_gr)$transcript <- names(isoseq_gr)
+isoseq_track <- GeneRegionTrack(isoseq_gr, group = "transcript", name = "Novel")
+
+displayPars(isoseq_track) <- list(
+  stacking = "squish",
+  fill = "#E69F00",
+  col = "#E69F00",
+  lwd = 0.3,
+  col.line = "black",
+  showId = TRUE,
+  background.panel = "transparent",
+  transcriptAnnotation = "transcript",
+  fontcolor.title = "black",
+  background.title = "#045a8d"
+)
+
 # Riboseq track
 
 riboseq_track <- AlignmentsTrack(
   "/scratch/s/shreejoy/ehogan/sfari/riboseq/analysis/peak-calling/merged.sorted.bam",
   name = "Ribo-seq",
   type = "coverage",
-  col = "blue",
-  fill = "blue",
-  alpha = 0.5,
-  lwd = 2
+  isPaired = FALSE
 )
 
 displayPars(riboseq_track) <- list(
@@ -131,17 +135,20 @@ displayPars(riboseq_track) <- list(
   showId = TRUE,
   transcriptAnnotation = "transcript",
   fontcolor.title = "black",
-  background.title = "#ef6548"
+  background.title = "#ef6548",
+  coverageHeight = 0.8,
+  maxCoverageHeight=10
 )
 
 # Combine all tracks
+
+chr <- as.character(seqnames(isoseq_gr))[1]
 
 axisTrack <- GenomeAxisTrack()
 ideoTrack <- IdeogramTrack(genome = "hg38", chromosome = chr)
 
 leftmost <- isoseq_gr@ranges@start %>% min()
-rightmost <- isoseq_gr@ranges@end  %>% max()
-
+rightmost <- end(isoseq_gr)  %>% max()
 extra <- (rightmost - leftmost) * 0.05
 
 pdf(glue("figures/figure_2/genome_track_{current_gene}.pdf"), width = 9, height = 3)
@@ -153,7 +160,7 @@ plotTracks(
   from = leftmost - extra,
   to = rightmost + extra,
   sizes = c(
-    1, 1, 1, 1, 1
+    1, 1, 1, 1, 1, 1
   )
 )
 dev.off()
