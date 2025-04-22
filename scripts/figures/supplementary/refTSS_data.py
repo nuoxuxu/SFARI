ae6b5e5f439619444dae40612bbc3c42eedfef23
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-04-22 12:52:09 -0400

    Add CAGE and polyA peak annotations to classification

--- scripts/figures/supplementary/refTSS_data.py
+++ scripts/figures/supplementary/refTSS_data.py
@@ -1,10 +1,19 @@
 import polars as pl
 from src.utils import read_gtf
 
+# Read in datasets
 classification = pl.read_parquet("nextflow_results/V47/final_classification.parquet")
 reftss = pl.read_csv("data/liftovered_mm39_to_hg38_peaks_overlapped_reftss_hg38_500bp.bed", separator="\t", has_header = False, new_columns=["chrom", "start", "end", "name", "score", "strand"])
+polyA_site = pl.read_csv(
+    "data/atlas.clusters.2.0.GRCh38.96.bed", 
+    separator="\t", new_columns=["chrom", "start", "end", "name", "score", "strand"], 
+    schema_overrides={"chrom": pl.String})\
+    .with_columns(
+        pl.col("chrom").map_elements(lambda x: "".join(["chr", x]), return_dtype=pl.String).alias("chrom")
+    )
 gtf = read_gtf("nextflow_results/V47/final_transcripts.gtf")
 
+# Get list of tx validated by CAGE-seq peaks
 validated_pbids = gtf\
     .filter(pl.col("feature")=="transcript")\
     .select(
@@ -25,6 +34,7 @@ validated_pbids = gtf\
     .unique("transcript_id")\
     ["transcript_id"].to_list()
 
+# Get percentage of isoforms validated by CAGE-seq peaks for visualization
 classification\
     .with_columns(
         CAGE = pl.col("isoform").is_in(validated_pbids),
@@ -48,3 +58,80 @@ classification\
     ]).with_columns(
         (pl.col("true_len") / pl.col("total_len") * 100).alias("CAGE_percentage")
     ).write_csv("data/structural_category_cage_percentage.csv")
+
+# Annotate classification with CAGE peak information
+classification = classification\
+    .with_columns(
+        within_CAGE_peak = pl.col("isoform").is_in(validated_pbids)
+    )
+
+# Get list of tx validated by polyA peaks
+validated_pbids = gtf\
+    .filter(pl.col("feature")=="transcript")\
+    .select(
+        pl.col("seqname"),
+        pl.col("transcript_id"),
+        pos = pl.when(pl.col("strand")=="+")\
+            .then(pl.col("end"))\
+            .otherwise(pl.col("start"))
+    )\
+    .join_where(
+        polyA_site,
+        (pl.col("pos") >= (pl.col("start"))) &
+        (pl.col("pos") <= (pl.col("end")))
+    )\
+    .filter(
+        pl.col("seqname")==pl.col("chrom")
+    ).unique("transcript_id")\
+    ["transcript_id"].to_list()
+
+# Get percentage of isoforms validated by polyA peaks for visualization
+classification\
+    .with_columns(
+        polyA_site = pl.col("isoform").is_in(validated_pbids),
+        structural_category2 = pl.when(pl.col("structural_category").is_in(["full-splice_match", "incomplete-splice_match", "novel_in_catalog", "novel_not_in_catalog"]))\
+            .then(pl.col("structural_category"))\
+            .otherwise(pl.lit("Other"))
+    )\
+    .with_columns(
+        structural_category2 = pl.when(pl.col("subcategory") == "3prime_fragment")\
+            .then(pl.lit("3prime_fragment"))\
+            .when(pl.col("subcategory") == "5prime_fragment")\
+            .then(pl.lit("5prime_fragment"))\
+            .otherwise(pl.col("structural_category2"))
+    )\
+    .group_by(["structural_category2", "polyA_site"])\
+    .len()\
+    .group_by("structural_category2")\
+    .agg([
+        pl.col("len").filter(pl.col("polyA_site") == True).sum().alias("true_len"),
+        pl.col("len").sum().alias("total_len")
+    ]).with_columns(
+        (pl.col("true_len") / pl.col("total_len") * 100).alias("polyA_site_percentage")
+    ).write_csv("data/structural_category_polyA_site_percentage.csv")    
+
+# Annotate classification with polyA peak information
+classification = classification\
+    .with_columns(
+        within_polyA_site = pl.col("isoform").is_in(validated_pbids)
+    )
+
+five_prime_fragment_with_polyA = classification\
+    .filter(
+        pl.col("subcategory") == "5prime_fragment",
+        pl.col("within_polyA_site")
+    )["isoform"].to_list()
+    
+three_prime_fragment_with_CAGE = classification\
+    .filter(
+        pl.col("subcategory") == "3prime_fragment",
+        pl.col("within_CAGE_peak")
+    )["isoform"].to_list()
+
+ISM_to_keep = five_prime_fragment_with_polyA + three_prime_fragment_with_CAGE
+
+classification\
+    .filter(
+            (pl.col("structural_category") != "incomplete-splice_match") | (pl.col("isoform").is_in(ISM_to_keep))
+        )\
+    .write_parquet("nextflow_results/V47/final_classification.parquet")
