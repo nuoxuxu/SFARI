e7ae0388312a2d3395b4d878df241ac82cbd8042
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-04-25 02:18:45 -0400

    Add scripts for DeepTMHMM, SignalP, DeepLoc, and IUPred to scipts/IsoformSwitchAnalyzeR

--- scripts/submit_sqanti3_qc.sh
+++ scripts/submit_sqanti3_qc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=sqanti3_qc
-#SBATCH --output=sqanti3_qc.out
-#SBATCH --time=0-12:0
+#SBATCH --output=slurm_logs/sqanti3_qc.out
+#SBATCH --time=0-6:0
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 
-conda activate SQANTI3.env
+mamba activate SQANTI3.env
 python /home/s/shreejoy/nxu/tools/SQANTI3/sqanti3_qc.py \
--t 40 \
---skipORF \
---output SQANTI3_qc \
---CAGE_peak "${GENOMIC_DATA_DIR}"/Human_hg38_Gencode_v39/refTSS_v3.3_human_coordinate.hg38.sorted.bed \
---polyA_motif_list "${GENOMIC_DATA_DIR}"/Human_hg38_Gencode_v39/polyA.list.txt \
-proc/merged_collapsed.filtered.gff "${GENOMIC_DATA_DIR}"/Human_hg38_Gencode_v39/gencode.v39.annotation.sorted.gtf "${GENOMIC_DATA_DIR}"/GENCODE/GRCh38.primary_assembly.genome.fa
+    -t 40 \
+    --skipORF \
+    --output SQANTI3_qc \
+    --CAGE_peak "${GENOMIC_DATA_DIR}"/Human_hg38_Gencode_v39/refTSS_v3.3_human_coordinate.hg38.sorted.bed \
+    --polyA_motif_list "${GENOMIC_DATA_DIR}"/Human_hg38_Gencode_v39/polyA.list.txt \
+    "nextflow_results/V47/merged_collapsed.sorted.filtered_lite.gff" "${GENOMIC_DATA_DIR}/GENCODE/gencode.v47.annotation.sorted.gtf" "${GENOMIC_DATA_DIR}/GENCODE/GRCh38.primary_assembly.genome.fa"
