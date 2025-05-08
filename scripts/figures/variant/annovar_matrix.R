a2d177bce35608eeba6dd69399cfb539683e9260
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-05-08 09:51:53 -0400

    Add R script for generating scatterpie plot

---
+++ scripts/figures/variant/annovar_matrix.R
@@ -0,0 +1,87 @@
+library(readr)
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(scatterpie)
+
+original <- read_excel("data/mmc2.xlsx", sheet="Table S2C", skip=1) %>% 
+    separate(
+        col   = Variant,
+        into  = c("chr", "pos", "ref", "alt"),
+        sep   = ":",
+        convert = TRUE
+    ) %>% 
+    mutate(
+        chr = gsub("chr", "", chr)
+    ) %>%
+    # insert a column identical to pos next to pos
+    mutate(
+        pos2 = pos
+    ) %>%
+    filter(nchar(ref) == 1)
+
+orfanage_predicted <- read_tsv("export/variant/annovar/de_novo/orfanage/.hg38_multianno.txt")
+gencode_predicted <- read_tsv("export/variant/annovar/de_novo/gencode/.hg38_multianno.txt")
+
+func_to_be_replaced <- orfanage_predicted %>%
+    distinct(Func.refGene, ExonicFunc.refGene) %>% 
+    filter(ExonicFunc.refGene ==".") %>% 
+    pull(Func.refGene)
+
+orfanage_predicted <- orfanage_predicted %>% 
+    mutate(
+        orfanage_variant_type = case_when(
+            Func.refGene %in% func_to_be_replaced ~ Func.refGene,
+            .default = ExonicFunc.refGene
+        )
+    )
+    
+gencode_predicted <- gencode_predicted %>%
+    mutate(
+        gencode_variant_type = case_when(
+            Func.refGene %in% func_to_be_replaced ~ Func.refGene,
+            .default = ExonicFunc.refGene
+        )
+    )
+
+bind_cols(original[, "ASD status"], gencode_predicted[, "gencode_variant_type"], orfanage_predicted[, "orfanage_variant_type"]) %>%
+    group_by(gencode_variant_type, orfanage_variant_type, `ASD status`) %>%
+    summarise(
+        n = n(),
+        .groups = "drop"
+    ) %>%
+    # long to wide
+    pivot_wider(
+        names_from = `ASD status`,
+        values_from = n,
+        values_fill = 0
+    ) %>%
+    rename(
+        "control" = `1`,
+        "case" = `2`
+    ) %>% 
+    mutate(
+        gencode_variant_type = as.integer(factor(gencode_variant_type, levels = unique(gencode_variant_type))),
+        orfanage_variant_type = as.integer(factor(orfanage_variant_type levels = unique(orfanage_variant_type)))
+    )
+
+df$region=factor(1:nrow(df))
+df$total=log(df$case+df$control+1,2) ^ 0.5 / 6.5
+
+df %>%
+    ggplot() +
+    geom_scatterpie(aes(x=variant_type, y=`ExonicFunc.refGene`, r=total, group=region), cols=c("case", "control")) +
+    scale_x_continuous(breaks = seq(1,23))+
+    scale_y_continuous(breaks = seq(1,9))+
+    coord_fixed()+
+    theme_bw(base_size=16) +
+    theme(legend.position = "bottom",
+            text = element_text(family = "Arial"),
+            panel.grid.major = element_line(size=0.5),
+            panel.grid.minor = element_blank(),
+            axis.title = element_text(face = "bold"),
+            axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    ylab("Reassigned VEP Consequence") +
+    xlab("Previous VEP Consequence")    
+
+
