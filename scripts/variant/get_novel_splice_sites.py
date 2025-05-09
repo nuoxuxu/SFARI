b925466051fc34fabb1fd74993e07014ff01cb04
Nuo Xu <xv.nuo@icloud.com>
2025-05-09 12:14:27 -0400

    Add option to filter for CDS SJ in novel canonical splice sitetes

--- scripts/variant/get_novel_splice_sites.py
+++ scripts/variant/get_novel_splice_sites.py
@@ -2,46 +2,59 @@ import polars as pl
 from src.utils import gtf_to_SJ, read_gtf
 import os
 
-final_transcripts_SJ = read_gtf("nextflow_results/V47/orfanage/orfanage.gtf")\
-    .filter(pl.col("feature") == "exon")\
-    .pipe(gtf_to_SJ)\
-    .unpivot(
-        index=["chrom", "strand", "transcript_id"],
-        variable_name="start_or_end",
-        value_name="coord"
-    )\
-    .unique(["chrom", "start_or_end", "coord"]).drop("strand")
-
-GENCODE_SJ = read_gtf("".join([os.getenv("GENOMIC_DATA_DIR"), "/GENCODE/gencode.v47.annotation.gtf"]))\
-    .filter(pl.col("feature") == "exon")\
-    .pipe(gtf_to_SJ)\
-    .unpivot(
-        index=["chrom", "strand", "transcript_id"],
-        variable_name="start_or_end",
-        value_name="coord"
-    )\
-    .unique(["chrom", "start_or_end", "coord"]).drop("strand")
-
-novel_splice_sites = final_transcripts_SJ\
-    .join(
-        GENCODE_SJ, 
-        on=["chrom", "start_or_end", "coord"], 
-        how="left",
-        coalesce=True
+def get_novel_CSSs(predicted_cds_gtf, out, feature="exon"):
+    """
+    Get novel canonical splice sites present in the predicted GTF but not the GENCODE GTF.
+    Args:
+        predicted_cds_gtf (str): Path to the predicted GTF file.
+        out (str): Path to the output file.
+        feature (str): Feature to filter on. Default is "exon".
+    """
+    
+    final_transcripts_SJ = read_gtf(predicted_cds_gtf)\
+        .filter(pl.col("feature") == feature)\
+        .pipe(gtf_to_SJ)\
+        .unpivot(
+            index=["chrom", "strand", "transcript_id"],
+            variable_name="start_or_end",
+            value_name="coord"
         )\
-    .filter(pl.col("transcript_id_right").is_null())\
-    .drop("transcript_id_right")\
-    .with_columns(
-        pl.when(pl.col("start_or_end") == "start")\
-            .then(pl.col("coord") + 1)\
-            .otherwise(pl.col("coord") - 1).alias("coord_1")
-    )
+        .unique(["chrom", "start_or_end", "coord"]).drop("strand")
 
-novel_splice_sites = novel_splice_sites\
-    .with_columns(
-        pl.when(pl.col("start_or_end") == "start").then(pl.col("coord")).otherwise(pl.col("coord_1")).alias("start"),
-        pl.when(pl.col("start_or_end") == "end").then(pl.col("coord")).otherwise(pl.col("coord_1")).alias("end")
-    )\
-    .drop("start_or_end", "coord", "coord_1")
+    gencode_gtf = os.getenv("GENOMIC_DATA_DIR") + "/GENCODE/gencode.v47.annotation.gtf" # type: ignore
+    GENCODE_SJ = read_gtf(gencode_gtf)\
+        .filter(pl.col("feature") == feature)\
+        .pipe(gtf_to_SJ)\
+        .unpivot(
+            index=["chrom", "strand", "transcript_id"],
+            variable_name="start_or_end",
+            value_name="coord"
+        )\
+        .unique(["chrom", "start_or_end", "coord"]).drop("strand")
 
-novel_splice_sites.write_csv("export/variant/novel_splice_sites.csv")
+    novel_splice_sites = final_transcripts_SJ\
+        .join(
+            GENCODE_SJ, 
+            on=["chrom", "start_or_end", "coord"], 
+            how="left",
+            coalesce=True
+            )\
+        .filter(pl.col("transcript_id_right").is_null())\
+        .drop("transcript_id_right")\
+        .with_columns(
+            pl.when(pl.col("start_or_end") == "start")\
+                .then(pl.col("coord") + 1)\
+                .otherwise(pl.col("coord") - 1).alias("coord_1")
+        )
+
+    novel_splice_sites = novel_splice_sites\
+        .with_columns(
+            pl.when(pl.col("start_or_end") == "start").then(pl.col("coord")).otherwise(pl.col("coord_1")).alias("start"),
+            pl.when(pl.col("start_or_end") == "end").then(pl.col("coord")).otherwise(pl.col("coord_1")).alias("end")
+        )\
+        .drop("start_or_end")
+
+    novel_splice_sites.write_csv(out)
+
+get_novel_CSSs("nextflow_results/V47/orfanage/orfanage.gtf", "export/variant/novel_splice_sites_all.csv", feature="exon")
+get_novel_CSSs("nextflow_results/V47/orfanage/orfanage.gtf", "export/variant/novel_splice_sites_cds.csv", feature="CDS")
