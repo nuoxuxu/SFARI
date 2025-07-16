78c1733e22c88fe570daa2c5c59386e77540ff95
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-07-15 23:00:56 -0400

    Add GTF file generation for novel exonic regions  in the CDS for Jimmy's Ribo-seq analysis

--- bin/novel_exonic_regions.R
+++ bin/novel_exonic_regions.R
@@ -56,3 +56,39 @@ as_tibble(novel_exonic_regions) %>%
     dplyr::select(-c(gene_id, transcript_id)) %>% 
     dplyr::select(seqnames, source, feature, start, end, score, strand, frame, attributes) %>% 
     write_tsv(output, col_names = FALSE, quote="none", escape="none")
+
+# Making GTF file for Jimmy's Ribo-seq analysis
+
+annotation_gtf <- paste0(Sys.getenv("GENOMIC_DATA_DIR"), "GENCODE/gencode.v47.annotation.gtf")
+predicted_cds_gtf <- "nextflow_results/V47/orfanage/orfanage.gtf"
+sequence_feature <- "CDS"
+
+SFARI_CDS <- rtracklayer::import(predicted_cds_gtf) %>%
+    subset(type == sequence_feature) %>% 
+    unique()
+
+SFARI_CDS_nonunique <- rtracklayer::import(predicted_cds_gtf) %>%
+    subset(type == sequence_feature)
+
+# GenomicRanges::subtract(SFARI_CDS, gencode_CDS)
+# SFARI_CDS_grl <- split(SFARI_CDS, SFARI_CDS$transcript_id, drop = TRUE)
+
+gencode_CDS <- makeTxDbFromGFF(annotation_gtf) %>%
+    cdsBy(by = "tx", use.names = TRUE) %>%
+    unlist() %>%
+    unique()
+
+novel_exonic_regions_CDS <- GenomicRanges::subtract(SFARI_CDS, gencode_CDS) %>% unlist() %>% unique() %>% reduce()
+
+hits <- findOverlaps(novel_exonic_regions_CDS, SFARI_CDS_nonunique)
+
+novel_exonic_regions_CDS_expanded <- novel_exonic_regions_CDS[queryHits(hits)]
+
+mcols(novel_exonic_regions_CDS_expanded)$transcript_id <- mcols(SFARI_CDS_nonunique[subjectHits(hits)])$transcript_id
+
+novel_exonic_regions_CDS_expanded_grl <- split(novel_exonic_regions_CDS_expanded, novel_exonic_regions_CDS_expanded$transcript_id, drop=TRUE)
+
+rtracklayer::export(unlist(novel_exonic_regions_CDS_expanded_grl), "export/variant/novel_exonic_regions_CDS_expanded_grl.gtf")
+
+sum(lengths(novel_exonic_regions_CDS_expanded_grl)!=1)
+length(novel_exonic_regions_CDS_expanded_grl)
