38faf6005bd3e8d9968690b352e857bad544552c
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-08-28 01:43:22 -0400

    Refactor short_long_read_correlation_V1.R to improve data processing: update variable names, enhance long-read normalization, and ensure proper library usage.

--- scripts/figures/supplementary/short_long_read_correlation_V1.R
+++ scripts/figures/supplementary/short_long_read_correlation_V1.R
@@ -4,10 +4,10 @@ library(stringr)
 library(purrr)
 library(GenomicFeatures)
 library(arrow)
-library(dplyr)
 library(RColorBrewer)
 library(pheatmap)
 library(reticulate)
+library(dplyr)
 
 classification <- read_parquet("nextflow_results/V47/final_classification.parquet")
 
@@ -24,26 +24,32 @@ names(files) <- str_split(files, "/") %>%
 
 txi <- tximport(files, type = "salmon", tx2gene = tx2gene, txOut = TRUE, txIdCol = "isoform")
 
-sr_expression <- txi$counts %>% as_tibble(rownames = "isoform")
+sr_expression_tpm <- txi$abundance %>% as_tibble(rownames = "isoform")
 
 # Get long-read count matrix
 
 lr_expression <- read_parquet("nextflow_results/V47/final_expression.parquet") %>% 
-    rename(
+    dplyr::rename(
         NPC_1_2 = NPC_1_3,
         NPC_3_2 = NPC_3_3,
         CN_1_1 = CN_1_2,
         CN_1_2 = CN_1_3
     )
 
+lr_expression_cpm <- lr_expression %>%
+  mutate(across(
+    where(is.numeric),
+    ~ .x / sum(.x) * 1e6
+  ))
+
 # Get correlation matrix
 
-cor_matrix <- lr_expression %>%
-    left_join(sr_expression, join_by(isoform == isoform), suffix = c("", "_illumina")) %>%
+cor_matrix <- lr_expression_cpm %>%
+    left_join(sr_expression_tpm, join_by(isoform == isoform), suffix = c("", "_illumina")) %>%
     left_join(select(classification, c(structural_category, isoform)), join_by(isoform == isoform)) %>% 
     filter(structural_category=="full-splice_match") %>% 
     dplyr::select(where(is.numeric)) %>%
-    cor()
+    cor(method="pearson", use="complete.obs")
 
 sample_names <- c("iPSC_1", "iPSC_2", "iPSC_3", "CN_1_1", "CN_1_2", "CN_2_1", "CN_2_2", "CN_3_1", "CN_3_2", "NPC_1_1", "NPC_1_2", "NPC_2_1", "NPC_2_2", "NPC_3_1", "NPC_3_2")
 sample_names <- c(sample_names, str_c(sample_names, "_illumina"))
