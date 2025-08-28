482dbb6036980c622ff5f87f1726c420a0dbb964
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2025-08-27 23:10:44 -0400

    Update .gitignore and enhance phyloP_boxplot.R for improved data processing and visualization

--- scripts/figures/figure_6/phyloP_boxplot.R
+++ scripts/figures/figure_6/phyloP_boxplot.R
@@ -2,19 +2,98 @@ library(dplyr)
 library(readr)
 library(rtracklayer)
 library(ggplot2)
+library(tidyr)
 library(patchwork)
+library(stringr)
 
-# Set ggplot theme
+novel_maps_output <- read_tsv("/scratch/nxu/100KGP_splicing/nextflow_results/novel/maps_output.tsv") %>%
+    mutate(
+        type = "Novel"
+    ) %>%
+    filter(
+        region %in% c("Acceptor", "Donor"),
+        site >= -10
+    )
 
-my_theme <- theme_classic() +
+known_maps_output <- read_tsv("/scratch/nxu/100KGP_splicing/nextflow_results/known/maps_output.tsv") %>%
+    mutate(
+        type = "Known"
+    ) %>%
+    filter(
+        (site >= -10) | (region == "Coding")
+    )
+
+novel_coding_maps <- read_tsv("MAPS_output.csv") %>%
+    filter(
+        region_site == "CDS_novel"
+    ) %>%
+    mutate(
+        region_site = c("CDS_novel" = "Coding")[region_site],
+        type = "Novel",
+        csq = c("synonymous_variant" = "Synonymous", "missense_variant" = "Missense", "stop_gained" = "Nonsense")[csq]
+    ) %>%
+    dplyr::rename(
+        xlab = csq,
+        region = region_site
+    )
+
+maps_output <- bind_rows(novel_maps_output, known_maps_output, novel_coding_maps) %>%
+  mutate(
+    zscore  = maps / se,
+    p_value = 2 * pnorm(abs(zscore), lower.tail = FALSE)
+  ) %>%
+  mutate(
+    p_adj = p.adjust(p_value, method = "BH")
+  ) %>% 
+  mutate(
+    p_value = ifelse(p_adj < 0.05, TRUE, FALSE)
+  ) %>% 
+  mutate(site = as.character(site)) %>% 
+  mutate(site = coalesce(site, xlab)) %>% 
+    mutate(
+        site = factor(site, levels = c("-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+        "Synonymous", "Missense", "Nonsense"))
+    )
+
+my_theme <- theme_bw() +
     theme(
-        axis.title.x = element_text(size = 13),
-        axis.title.y = element_text(size = 13),
-        axis.text.x = element_text(size = 12),
-        axis.text.y = element_text(size = 12)
+        axis.text.x = element_text(size = 20, vjust = 0.5, angle = 90, hjust = 1, color = "black"),
+        axis.text.y = element_text(size = 20, color = "black"),
+        axis.title.y = element_text(size = 25, color = "black"),
+        strip.text = element_text(size = 20, color = "black"),
+        strip.placement = "outside",
+        legend.title = element_blank(),
+        legend.position = c(0.08, 0.90),
+        legend.text = element_text(size = 20),
+        # panel.grid.major.x = element_line(size = 1),
+        # panel.spacing.y = unit(1, "lines")
     )
 
 theme_set(my_theme)
+
+maps_plot <- maps_output %>%
+    mutate(region = factor(region, levels = c("Acceptor", "Donor", "Coding"))) %>%
+    ggplot(aes(site, maps, color = type)) +
+    geom_pointrange(
+        aes(
+            ymin = maps - 1.96 * se,
+            ymax = maps + 1.96 * se,
+            shape = p_value
+        ),
+        size = 0.8
+    ) +
+    geom_hline(yintercept = 0.051, linetype = 3, color = "#00BFC4", size = 1.2) +
+    ylab("Mutational Constraint (MAPS)") +
+    scale_color_manual(values = c("Novel" = "#F8766D", "Known" = "#00BFC4")) +
+    scale_shape_manual(values = c(`FALSE` = 16, `TRUE` = 1)) +
+    scale_x_discrete(breaks = levels(maps_output$site)[seq(1, length(levels(maps_output$site)), by = 5)]) +
+    facet_grid(cols=vars(region), rows=vars(type), scales = "free", space="free_x") +
+    theme(
+        legend.position = "none",
+        axis.title.x = element_blank(),
+        strip.text = element_blank(),
+        strip.background = element_blank(),
+    )
 
 # Essential splice sites
 
@@ -49,7 +128,7 @@ combined_exonic_regions <- bind_rows(CDS_regions, UTR_regions) %>%
 
 df <- bind_rows(splice_sites_df, combined_exonic_regions)
 
-df %>%
+phyloP_boxplot <- df %>%
     mutate(column = factor(column, levels = c("donor", "acceptor", "CDS", "UTR"))) %>%
     ggplot(aes(column, phyloP, fill = type)) +
     geom_boxplot(alpha = 0.5, outliers=FALSE) +
@@ -57,17 +136,58 @@ df %>%
                colour = "red",
                linetype = "dashed",
                linewidth = 1) +
-    labs(x = NULL) +
+    labs(x = NULL, y="Evolutionary Conservation (phyloP)") +
     ylim(c(-8, 10))
 
-df %>% 
-    filter(column=="donor", type=="known") %>% 
-    mutate(phyloP = ifelse(phyloP < 2.27, TRUE, FALSE)) %>%
-    summarise(percentage = sum(phyloP) / n() * 100)
+maps_plot + phyloP_boxplot + plot_layout(widths = c(1.5, 1))
+ggsave("figures/figure_6/phyloP_boxplot.pdf", width = 14, height = 8)
 
-df %>% 
-    filter(column=="acceptor", type=="known") %>% 
-    mutate(phyloP = ifelse(phyloP < 2.27, TRUE, FALSE)) %>%
-    summarise(percentage = sum(phyloP) / n() * 100)    
+combined_odds_conserved_vars <- read_tsv("data/combined_odds_conserved_vars.tsv") %>% 
+    mutate(
+        xlab = case_when(
+            str_detect(Category, "_") ~ str_replace(Category, "^[^_]*_", ""),
+            str_detect(Category, " ") ~ str_replace(Category, "^[^ ]* ", ""),
+            TRUE ~ Category
+        ),
+        color = case_when(
+            str_detect(Category, "Known") ~ "Known",
+            str_detect(Category, "Novel") ~ "Novel",
+            TRUE ~ "Other"
+        )
+    ) %>% 
+    pivot_longer(c(Affected_with, Unaffected_with), names_to="status", values_to="number") %>% 
+    filter(variant_type=="denovo") %>% 
+    mutate(
+        xlab = factor(xlab, levels = c("Acceptor", "Donor", "nonsense", "combined"))
+    ) %>% 
+    mutate(
+      xlab = recode(xlab, "nonsense" = "Nonsense", "combined" = "Combined"),
+      status = recode(status, "Affected_with" = "Affected", "Unaffected_with" = "Unaffected"),
+      Pvalue = ifelse(Pvalue < 0.05, format(Pvalue, scientific = TRUE, digits = 2), round(Pvalue, 2))
+    )
 
-ggsave("figures/figure_6/phyloP_boxplot.pdf", width = 8, height = 5.5)
+known_vars_plot <- combined_odds_conserved_vars %>% 
+    filter(color=="Known") %>% 
+    ggplot(aes(x=xlab, y=number, fill=status)) +
+    geom_col(position = position_dodge()) +
+    scale_fill_manual(values = c("Affected" = "#fdc086", "Unaffected" = "#beaed4")) +
+    geom_text(aes(label = paste0("OR = ", round(OddsRatio, 2))), vjust = -0.5, size = 6) +
+    geom_text(aes(label = paste0("p = ", Pvalue)), vjust = -2, size = 6) +
+    labs(x = NULL, y = "Individuals with >= 1 de novo variants") +
+    ylim(c(0, 2400))
+    
+novel_vars_plot <- combined_odds_conserved_vars %>% 
+    filter(color=="Novel") %>% 
+    ggplot(aes(x=xlab, y=number, fill=status)) +
+    geom_col(position = position_dodge()) +
+    scale_fill_manual(values = c("Affected" = "#fdc086", "Unaffected" = "#beaed4")) +
+    geom_text(aes(label = paste0("OR = ", round(OddsRatio, 2))), vjust = -0.5, size=6) +
+    geom_text(aes(label = paste0("p = ", Pvalue)), vjust = -2, size=6) +
+    labs(x = NULL, y = NULL) +
+    theme(
+        legend.position = "none"
+    ) +
+    ylim(c(0, 60))
+
+known_vars_plot + novel_vars_plot
+ggsave("figures/figure_6/combined_odds_plot.pdf", width = 12, height = 5.5)
