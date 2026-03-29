b470df82f4257725f50289d0501d0ac6f339af6d
Nuo Xu <nuoxu.xu@mail.utoronto.ca>
2026-03-29 02:10:21 -0400

    Update run_bambu.R. The original did not work

--- bin/run_bambu.R
+++ bin/run_bambu.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 library(bambu)
+library(rtracklayer)
+library(arrow)
+library(readr)
 
 args = commandArgs(trailingOnly=TRUE)
 annotation_gtf <- args[1]
@@ -12,14 +15,24 @@ bambuAnnotations <- prepareAnnotations(annotation_gtf)
 bam_files <- Sys.glob(aligned_bam)
 
 bambu_result <- bambu::bambu(
-    reads = bam_files, 
+    reads = bam_files,
     annotations = bambuAnnotations,
     genome = ref_genome_fasta,
-    ncore = ncore, 
+    ncore = ncore,
     opt.discovery = list(remove.subsetTx = FALSE, min.readCount=1),
-    quant = FALSE, 
+    quant = FALSE,
     discovery = TRUE,
-    lowMemory = TRUE,
+    processByChromosome = TRUE,
 )
 
 saveRDS(bambu_result, output_rds)
+
+do.call(cbind, lapply(bambu_result, function(df) assays(df)$counts)) %>% 
+    as.matrix() %>% 
+    write.csv("nextflow_results/comapre_other_LRS_tools/bambu/bambu_expression.csv")
+
+bambu_result <- bambu_result[[1]]
+supportedTx <- !is.na(rowData(bambu_result)$readCount)
+writeToGTF(rowRanges(bambu_result)[supportedTx, ], file = "nextflow_results/comapre_other_LRS_tools/bambu/supportedTranscriptModels.gtf")
+data.frame(rowData(bambu_result)[supportedTx, ]) %>% 
+    write_tsv(file="nextflow_results/comapre_other_LRS_tools/bambu/supportedTxClassification.txt")
