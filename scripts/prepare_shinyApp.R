cff7dc13d9848011a618f5b647ce160ffdd57984
Nuo Xu <xv.nuo@icloud.com>
2026-05-10 07:56:52 -0400

    Refactor Shiny app to include coding status filter and update data processing

--- scripts/prepare_shinyApp.R
+++ scripts/prepare_shinyApp.R
@@ -11,19 +11,14 @@ structural_category_labels <- c(
     "Other"                    = "Other"
 )
 
-time_point_labels <- c(
-  "mean_iPSC" = "t00",
-  "mean_NPC"  = "t04",
-  "mean_CN"   = "t30"
-)
-
-classification <- read_parquet("nextflow_results/V47/final_classification.parquet") %>% 
+classification <- read_parquet("nextflow_results/V47/final_classification.parquet") %>%
   mutate(
     structural_category = if_else(structural_category %in% c("full-splice_match", "incomplete-splice_match", "novel_in_catalog", "novel_not_in_catalog"), structural_category, "Other"),
     structural_category = structural_category_labels[structural_category],
     structural_category = factor(structural_category, levels = c("FSM", "ISM", "NIC", "NNC", "Other"))
   )
 
+# Keep per-replicate data (not just means) so the app can show variance
 lr_log2_cpm <- read_parquet("nextflow_results/V47/final_expression.parquet") %>%
   dplyr::rename(
     NPC_1_2 = NPC_1_3,
@@ -33,28 +28,26 @@ lr_log2_cpm <- read_parquet("nextflow_results/V47/final_expression.parquet") %>%
   ) %>%
   mutate(
     across(
-      where(is.numeric), 
+      where(is.numeric),
       ~ log2((.x / sum(.x) * 1e6) + 1)
     )
-  ) %>% 
-  mutate(
-    mean_iPSC = rowMeans(select(., starts_with("iPSC_")), na.rm = TRUE),
-    mean_NPC  = rowMeans(select(., starts_with("NPC_")), na.rm = TRUE),
-    mean_CN   = rowMeans(select(., starts_with("CN_")), na.rm = TRUE)
-  ) %>% 
-  select(isoform, mean_iPSC, mean_NPC, mean_CN) %>% 
+  ) %>%
   pivot_longer(
-    cols = c("mean_iPSC", "mean_NPC", "mean_CN"),
-    names_to = "time_point",
-    values_to = "abundance"
-  ) %>% 
+    cols = -isoform,
+    names_to = "replicate",
+    values_to = "log2_cpm"
+  ) %>%
+  mutate(
+    time_point = case_when(
+      startsWith(replicate, "iPSC_") ~ "t00",
+      startsWith(replicate, "NPC_")  ~ "t04",
+      startsWith(replicate, "CN_")   ~ "t30"
+    ),
+    time_point = factor(time_point, levels = c("t30", "t04", "t00"))
+  ) %>%
   left_join(
     classification[, c("isoform", "structural_category", "associated_gene")],
     by = "isoform"
-  ) %>%
-  mutate(
-    time_point = time_point_labels[time_point],
-    time_point = factor(time_point, levels = c("t30", "t04", "t00"))
   )
 
 orfanage_gtf <- rtracklayer::import("nextflow_results/V47/orfanage/orfanage.gtf") %>% as.data.frame()
@@ -74,5 +67,20 @@ gtf <- gtf %>%
   ) %>%
   filter(!(type %in% c("five_prime_utr", "three_prime_utr", "start_codon", "stop_codon", "gene")))
 
+# Flag transcripts that have CDS features (i.e. predicted protein-coding)
+gtf <- gtf %>%
+  mutate(
+    has_CDS = case_when(!transcript_id %in% tx_no_CDS ~ TRUE, .default = FALSE)
+  )
+
+# Propagate has_CDS flag to expression data
+cds_flag <- gtf %>%
+  distinct(transcript_id, has_CDS) %>%
+  dplyr::rename(isoform = transcript_id)
+
+lr_log2_cpm <- lr_log2_cpm %>%
+  left_join(cds_flag, by = "isoform")
+
+dir.create("transcript_vis_app/data", showWarnings = FALSE)
 lr_log2_cpm %>% write.csv("transcript_vis_app/data/lr_log2_cpm.csv", row.names = FALSE)
 gtf %>% write.csv("transcript_vis_app/data/gtf.csv", row.names = FALSE)
--- transcript_vis_app/app.R
+++ transcript_vis_app/app.R
@@ -36,24 +36,34 @@ pastelColorVector <- c(
     "Other" = "#7F7F7F"
 )
 
+time_point_colors <- c("t00" = "#e0a19c", "t04" = "#e14bd5", "t30" = "#ff0011")
+
 gtf <- read.csv("data/gtf.csv")
-lr_log2_cpm <- read.csv("data/lr_log2_cpm.csv") %>% 
+lr_log2_cpm <- read.csv("data/lr_log2_cpm.csv") %>%
   mutate(
     structural_category = factor(structural_category, levels = c("FSM", "ISM", "NIC", "NNC")),
-    time_point = factor(time_point, levels = c("t30", "t04", "t00"))
+    time_point = factor(time_point, levels = c("t30", "t04", "t00")),
+    has_CDS = as.logical(has_CDS)
   )
 
-plot_ggtranscript <- function(gene_of_interest) {
-  exons <- gtf %>%
-    filter(
-      type == "exon",
-      associated_gene == {{ gene_of_interest }}
-    )
-  CDS <- gtf %>%
-    filter(
-      type == "CDS",
-      associated_gene == {{ gene_of_interest }}
-    )
+apply_coding_filter <- function(data, coding_filter, id_col = "isoform") {
+  if (coding_filter == "coding") {
+    data %>% filter(has_CDS == TRUE)
+  } else if (coding_filter == "non_coding") {
+    data %>% filter(has_CDS == FALSE)
+  } else {
+    data
+  }
+}
+
+plot_ggtranscript <- function(gene_of_interest, coding_filter = "all") {
+  gtf_gene <- gtf %>%
+    filter(associated_gene == gene_of_interest) %>%
+    apply_coding_filter(coding_filter)
+
+  exons <- gtf_gene %>% filter(type == "exon")
+  CDS <- gtf_gene %>% filter(type == "CDS")
+
   exons %>%
     ggplot(
       aes(xstart = start, xend = end, y = transcript_id, strand = strand)
@@ -79,31 +89,46 @@ plot_ggtranscript <- function(gene_of_interest) {
     facet_grid(structural_category ~ ., scales = "free", space = "free")
 }
 
-plot_abundance <- function(gene_of_interest) {
-  lr_log2_cpm %>% 
+plot_abundance <- function(gene_of_interest, coding_filter = "all") {
+  data <- lr_log2_cpm %>%
     filter(associated_gene == gene_of_interest) %>%
-    ggplot(aes(x = abundance, y = isoform, fill = time_point)) +
-    geom_col(position = "dodge") +
-    xlab("mean log2(CPM + 1)") +
-    ylab(gene_of_interest) +
+    apply_coding_filter(coding_filter)
+
+  data %>%
+    ggplot(aes(x = log2_cpm, y = isoform, color = time_point)) +
+    # Individual replicate points
+    geom_point(
+      position = position_dodge(width = 0.7),
+      alpha = 0.5,
+      size = 1.5
+    ) +
+    # Mean ± SEM
+    stat_summary(
+      fun.data = mean_se,
+      geom = "pointrange",
+      position = position_dodge(width = 0.7),
+      size = 0.6,
+      linewidth = 0.8
+    ) +
+    xlab("log2(CPM + 1)") +
     theme(
       axis.text.y = element_blank(),
       axis.title.x = element_text(size = 14),
     ) +
-    scale_fill_manual(breaks = c("t00", "t04", "t30"), values = c("#e0a19c", "#e14bd5", "#ff0011")) +
-    facet_grid(structural_category ~ ., scales = "free", space = "free") 
+    scale_color_manual("Time point", breaks = c("t00", "t04", "t30"), values = time_point_colors) +
+    facet_grid(structural_category ~ ., scales = "free", space = "free")
 }
 
-plot_combined <- function(gene_of_interest) {
-  ggtranscript_plot <- plot_ggtranscript(gene_of_interest)
-  abundance <- plot_abundance(gene_of_interest)
+plot_combined <- function(gene_of_interest, coding_filter = "all") {
+  ggtranscript_plot <- plot_ggtranscript(gene_of_interest, coding_filter)
+  abundance <- plot_abundance(gene_of_interest, coding_filter)
   ggtranscript_plot | abundance + plot_layout(widths = c(2, 1))
 }
 
-get_n_transcripts <- function(gene_of_interest) {
+get_n_transcripts <- function(gene_of_interest, coding_filter = "all") {
   gtf %>%
-    filter(type == "transcript") %>%
-    filter(associated_gene == gene_of_interest) %>%
+    filter(type == "transcript", associated_gene == gene_of_interest) %>%
+    apply_coding_filter(coding_filter) %>%
     nrow()
 }
 
@@ -117,8 +142,17 @@ ui <- fluidPage(
       choices = choices <- unique(gtf$associated_gene),
       selected = "AGO1"
     ),
+    selectInput(
+      "coding_filter",
+      "Filter isoforms by coding status:",
+      choices = c(
+        "All isoforms"              = "all",
+        "Protein-coding (has CDS)"  = "coding",
+        "Non-coding (no CDS)"       = "non_coding"
+      ),
+      selected = "all"
+    ),
     helpText("To view the genomic region of the selected gene in the UCSC Genome Browser, click the link below:"),
-    #insert a hyperlink here
     uiOutput("ucsc_link"),
     width = 3
   ),
@@ -131,19 +165,19 @@ server <- function(input, output, session) {
   output$combined_plot <- renderPlot(
     {
       validate(need(input$select_genes, 'Choose a gene!'))
-      plot_combined(input$select_genes)
+      plot_combined(input$select_genes, input$coding_filter)
     },
     height = reactive({
-      35 * sum(get_n_transcripts(input$select_genes)) + 100
+      35 * sum(get_n_transcripts(input$select_genes, input$coding_filter)) + 100
     })
   )
   output$ucsc_link <- renderUI({
     validate(need(input$select_genes, 'Choose a gene!'))
-  gene_info <- gtf %>%
-    filter(type == "transcript", associated_gene == input$select_genes)
-  chrom <- gene_info %>% select(seqnames) %>% distinct() %>% pull()
-  start <- gene_info %>% select(start) %>% min()
-  end <- gene_info %>% select(end) %>% max()
+    gene_info <- gtf %>%
+      filter(type == "transcript", associated_gene == input$select_genes)
+    chrom <- gene_info %>% select(seqnames) %>% distinct() %>% pull()
+    start <- gene_info %>% select(start) %>% min()
+    end <- gene_info %>% select(end) %>% max()
     url <- paste0("https://genome.ucsc.edu/s/nuoxuxu/iPSC_Neuron_Proteogenomic_atlas?position=", chrom, ":", start, "-", end)
     tags$a(href = url, "View in UCSC Genome Browser", target = "_blank")
   })
